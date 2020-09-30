ee25801af3b7c57c52df90e57837dd4e2eee8f38
Dylan Jean <dylanjean@gmail.com>
2020-09-29 22:06:01 -0400

    changes

--- static/js/app.js
+++ static/js/app.js
@@ -7,37 +7,40 @@ var tbody = d3.select("tbody");
 
 
 data.forEach(obj => {
-    var tRow = tbody.append("tr");
+    var tr = tbody.append("tr");
     Object.entries(obj).forEach(([key,value]) => {
-        var tData = tRow.append("td");
-        tData.text(value);
+        var data = tr.append("td");
+        data.text(value);
     });
 });
 
-var submitButton = d3.select("#filter-btn");
-var inputField = d3.select("#datetime");
+var button = d3.select("#filter-btn");
+var input = d3.select("#datetime");
 
-// implementing fuction to "filter button"
-submitButton.on("click", function() {
-    // clears the data of the current table        
+
+var form = d3.select("form");
+
+
+button.on("click", submit)
+
+function submit () {      
+    var input = d3.select("#datetime"); 
+    var inputValue = input.property("value");
+    
     tbody.html("");
     d3.event.preventDefault();
-    // select the input field
-    var inputField = d3.select("#datetime");
-    // get the value property of the "input" element 
-    var inputElement = inputField.property("value");
+    
 
 
-    var inputTypeArray = data.filter(one => one.datetime === inputElement);
-    console.log(inputTypeArray)
+    var userInput = tableData.filter(item => item.datetime === inputValue);
+    console.log(userInput)
  
-    inputTypeArray.forEach((selection) => {
-        // for each "element" create a row
-        var row = tbody.append("tr");
+    userInput.forEach((selection) => {
+        var tr2 = tbody.append("tr");
         Object.entries(selection).forEach(([key,value]) => {
-            var cell = row.append("td");
-            // adds the "value" to each row in the table
-            cell.text(value);
+            var td = tr2.append("td");
+            td.text(value);
         });
     });      
-});
+}
+form.on("submit",submit)
