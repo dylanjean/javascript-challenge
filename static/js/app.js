2925d159a45457623527dcc90e4db697be07dd07
Dylan Jean <dylanjean@gmail.com>
2020-10-01 14:05:03 -0400

    final changes

--- static/js/app.js
+++ static/js/app.js
@@ -9,8 +9,8 @@ var tbody = d3.select("tbody");
 data.forEach(obj => {
     var tr = tbody.append("tr");
     Object.entries(obj).forEach(([key,value]) => {
-        var data = tr.append("td");
-        data.text(value);
+        var td = tr.append("td");
+        td.text(value);
     });
 });
 
@@ -21,26 +21,20 @@ var input = d3.select("#datetime");
 var form = d3.select("form");
 
 
-button.on("click", submit)
+button.on("click", submit);
 
 function submit () {      
     var input = d3.select("#datetime"); 
-    var inputValue = input.property("value");
-    
-    tbody.html("");
     d3.event.preventDefault();
-    
+    tbody.html("");
 
-
-    var userInput = tableData.filter(item => item.datetime === inputValue);
-    console.log(userInput)
- 
-    userInput.forEach((selection) => {
+    var userInput = tableData.filter(item => item.datetime === input.property("value"));
+    userInput.forEach((entry) => {
         var tr2 = tbody.append("tr");
-        Object.entries(selection).forEach(([key,value]) => {
+        Object.entries(entry).forEach(data => {
             var td = tr2.append("td");
-            td.text(value);
+            td.text(data);
         });
     });      
-}
-form.on("submit",submit)
+};
+form.on("submit",submit);
